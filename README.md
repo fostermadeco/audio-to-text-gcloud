cc0b9c0dd458578f36da838e0ac966fb17869f5e
Kelly Packer <kelly@kellypacker.com>
2019-02-21 12:40:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Overview
+Audio-to-Text is a Google Cloud Function that sends an audio file to the Google Cloud Speech API for translation. The Cloud function is triggered by posting a file to an HTTP endpoint.
+
+## Example Usage
+
+We've used this on a React Native app built with Expo. Expo records an audio file, which is sent to the Google Function.
+
+## Components
+The pipeline is built on two components:
+1. Google Cloud Functions
+1. Google Cloud Speech API
+
+You will need to sign up for Google Cloud services (including billing), create a project and enable the Speech API. To deploy the function from local, you will need to install the Google Cloud SDK. More on that [here](https://cloud.google.com/nodejs/getting-started/hello-world).
+
+## Audio Config
+One of the tricky parts about this is to match the config to the file you are trying to translate. On iOS I have had the best luck with `.wav` files at 41000 Hz.
+
+This Cloud Function includes a hardcoded config:
+- Format: LINEAR16
+- Frequency: 24000 Hz (16000 or higher is recommended)
+- Channels: Mono (a Cloud Speech API requirement)
+- Language: English (US)
+
+Included in this repo is a test audio file that matches this config. I also used [ffmpeg](https://www.ffmpeg.org/) to get the encoding, hZ, etc for an audio file:
+
+```
+brew install ffmpeg
+ffmpeg -i ~/Documents/speech-test.wav -hide_banner
+```
+
+## Deployment
+
+To create the function and deploy it:
+```
+gcloud functions deploy audioToText --trigger-http --runtime nodejs8
+```
+After it's created:
+
+```
+gcloud functions deploy audioToText
+```
+
+## References
+* https://cloud.google.com/functions/docs/writing/http - Multipart Data
+* Helpful example of audio to text pipeline: https://github.com/jlaham/audio-2-text
---
+++ index.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+// Node.js doesn't have a built-in multipart/form-data parsing library.
+// Instead, we can use the 'busboy' library from NPM to parse these requests.
+const Busboy = require('busboy');
+const Speech = require('@google-cloud/speech');
+
+const ENCODING = 'LINEAR16';
+const SAMPLE_RATE_HERTZ = 24000;
+const LANGUAGE = 'en-US';
+
+const audioConfig = {
+    encoding: ENCODING,
+    sampleRateHertz: SAMPLE_RATE_HERTZ,
+    languageCode: LANGUAGE,
+};
+
+const convertToText = (file, config) => {
+    console.log('FILE:', JSON.stringify(file));
+
+    const audio = {
+        content: fs.readFileSync(file).toString('base64'),
+    };
+
+    const request = {
+        config,
+        audio,
+    };
+
+    const speech = new Speech.SpeechClient();
+
+    return speech.recognize(request).then((response) => {
+        return response;
+    }).catch((error) => {
+        console.log('SPEECH error:', error);
+    });
+};
+
+/**
+ * Audio-to-Text is a Cloud Function that is triggered by an HTTP
+ * request. The function processes one audio file.
+ *
+ * @param {object} req Cloud Function request context.
+ * @param {object} res Cloud Function response context.
+ */
+exports.audioToText = (req, res) => {
+    if (req.method !== 'POST') {
+        res.status(405).end();
+    }
+
+    const busboy = new Busboy({ headers: req.headers });
+    const tmpdir = os.tmpdir();
+
+    let tmpFilePath;
+    let fileWritePromise;
+
+    // Process the file
+    busboy.on('file', (fieldname, file, filename) => {
+        // Note: os.tmpdir() points to an in-memory file system on GCF
+        // Thus, any files in it must fit in the instance's memory.
+        const filepath = path.join(tmpdir, filename);
+        tmpFilePath = filepath;
+
+        const writeStream = fs.createWriteStream(filepath);
+        file.pipe(writeStream);
+
+        // File was processed by Busboy; wait for it to be written to disk.
+        const promise = new Promise((resolve, reject) => {
+            file.on('end', () => {
+                writeStream.end();
+            });
+            writeStream.on('finish', resolve);
+            writeStream.on('error', reject);
+        });
+        fileWritePromise = promise;
+    });
+
+    // Triggered once the file is processed by Busboy.
+    // Need to wait for the disk writes to complete.
+    busboy.on('finish', () => {
+        fileWritePromise.then(() => {
+            convertToText(tmpFilePath, audioConfig).then((response) => {
+                const transcript = response[0].results
+                    .map(result => result.alternatives[0].transcript)
+                    .join('\n');
+                res.send({ transcript });
+            });
+            fs.unlinkSync(tmpFilePath);
+        });
+    });
+
+    busboy.end(req.rawBody);
+};
